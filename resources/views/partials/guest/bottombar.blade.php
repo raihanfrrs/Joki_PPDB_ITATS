88172f4347f41962b03751bb78ad0f76c5ed6668
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2024-11-29 07:03:41 +0700

    feat: authentication

---
+++ resources/views/partials/guest/bottombar.blade.php
@@ -0,0 +1,158 @@
+<aside id="layout-menu" class="layout-menu-horizontal menu-horizontal menu bg-menu-theme flex-grow-0">
+    <div class="container-xxl d-flex h-100">
+        <ul class="menu-inner d-flex justify-content-center align-items-center">
+            <!-- Dashboards -->
+            <li class="menu-item active">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons ti ti-smart-home"></i>
+                    <div data-i18n="Dashboards">Dashboards</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item active">
+                        <a href="index.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-chart-pie-2"></i>
+                            <div data-i18n="Analytics">Analytics</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="dashboards-crm.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-3d-cube-sphere"></i>
+                            <div data-i18n="CRM">CRM</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="dashboards-ecommerce.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-atom-2"></i>
+                            <div data-i18n="eCommerce">eCommerce</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- Apps -->
+            <li class="menu-item">
+                <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons ti ti-layout-grid-add"></i>
+                    <div data-i18n="Apps">Apps</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="app-email.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-mail"></i>
+                            <div data-i18n="Email">Email</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="app-chat.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-messages"></i>
+                            <div data-i18n="Chat">Chat</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="app-calendar.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-calendar"></i>
+                            <div data-i18n="Calendar">Calendar</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="app-kanban.html" class="menu-link">
+                            <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
+                            <div data-i18n="Kanban">Kanban</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:void(0);" class="menu-link menu-toggle">
+                            <i class="menu-icon tf-icons ti ti-file-dollar"></i>
+                            <div data-i18n="Invoice">Invoice</div>
+                        </a>
+                        <ul class="menu-sub">
+                            <li class="menu-item">
+                                <a href="app-invoice-list.html" class="menu-link">
+                                    <div data-i18n="List">List</div>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="app-invoice-preview.html" class="menu-link">
+                                    <div data-i18n="Preview">Preview</div>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="app-invoice-edit.html" class="menu-link">
+                                    <div data-i18n="Edit">Edit</div>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="app-invoice-add.html" class="menu-link">
+                                    <div data-i18n="Add">Add</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:void(0);" class="menu-link menu-toggle">
+                            <i class="menu-icon tf-icons ti ti-users"></i>
+                            <div data-i18n="Users">Users</div>
+                        </a>
+                        <ul class="menu-sub">
+                            <li class="menu-item">
+                                <a href="app-user-list.html" class="menu-link">
+                                    <div data-i18n="List">List</div>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                                    <div data-i18n="View">View</div>
+                                </a>
+                                <ul class="menu-sub">
+                                    <li class="menu-item">
+                                        <a href="app-user-view-account.html" class="menu-link">
+                                            <div data-i18n="Account">Account</div>
+                                        </a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="app-user-view-security.html" class="menu-link">
+                                            <div data-i18n="Security">Security</div>
+                                        </a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="app-user-view-billing.html" class="menu-link">
+                                            <div data-i18n="Billing & Plans">Billing & Plans</div>
+                                        </a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="app-user-view-notifications.html" class="menu-link">
+                                            <div data-i18n="Notifications">Notifications</div>
+                                        </a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="app-user-view-connections.html" class="menu-link">
+                                            <div data-i18n="Connections">Connections</div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item">
+                        <a href="javascript:void(0);" class="menu-link menu-toggle">
+                            <i class="menu-icon tf-icons ti ti-settings"></i>
+                            <div data-i18n="Roles & Permissions">Roles & Permission</div>
+                        </a>
+                        <ul class="menu-sub">
+                            <li class="menu-item">
+                                <a href="app-access-roles.html" class="menu-link">
+                                    <div data-i18n="Roles">Roles</div>
+                                </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="app-access-permission.html" class="menu-link">
+                                    <div data-i18n="Permission">Permission</div>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</aside>
