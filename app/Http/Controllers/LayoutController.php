3695af85d1c0ee5564d4044f26a32bf6904f07b3
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2025-02-11 07:07:38 +0700

    feat: dashboard student

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -2,23 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\RoleRepository;
 use Illuminate\Http\Request;
+use App\Repositories\RoleRepository;
 use Illuminate\Support\Facades\Auth;
+use App\Repositories\PaymentRepository;
+use App\Repositories\StudentRepository;
+use App\Repositories\RegistrationRepository;
 
 class LayoutController extends Controller
 {
-    protected $role;
+    protected $role, $student, $registration, $payment;
 
-    public function __construct(RoleRepository $role)
+    public function __construct(RoleRepository $role, StudentRepository $student, RegistrationRepository $registration, PaymentRepository $payment)
     {
         $this->role = $role;
+        $this->student = $student;
+        $this->registration = $registration;
+        $this->payment = $payment;
     }
 
     public function index()
     {
         if (Auth::check() && $this->role->find(auth()->user()->role_id)->role == 'student') {
-            return view('pages.student.dashboard.index');
+            return view('pages.student.dashboard.index', [
+                'student' => $this->student->find(auth()->user()->student->id),
+                'registration' => $this->registration->getRegistrationByStudentId(auth()->user()->student->id),
+                'fatherFilled' => $this->registration->checkIfFatherFilled(auth()->user()->student->id),
+                'payment' => $this->payment->getPaymentByStudentId(auth()->user()->student->id)
+            ]);
         } else if (Auth::check() && $this->role->find(auth()->user()->role_id)->role == 'admin') {
             return view('pages.admin.dashboard.index');
         } else if (Auth::check() && $this->role->find(auth()->user()->role_id)->role == 'principle') {
